3c8938d3ac676c567e15a9a3be0efe3b61cbeb0e
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2026-01-14 15:54:37 +0100

    Refactor

--- src/remote/remote_autofocus.py
+++ src/remote/remote_autofocus.py
@@ -1,21 +1,23 @@
 from flask import Flask, Response
 from picamera2 import Picamera2
 import time
+import numpy as np
 
 app = Flask(__name__)
 
+def take_photo() -> np.ndarray:
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_still_configuration())
+    picam2.start()
+    time.sleep(1)
+    array = picam2.capture_array("main")
+    picam2.stop()
+    return array
 
 @app.route("/rate-sharpness", methods=["GET"])
 def rate_sharpness_route():
-    try:
-        picam2 = Picamera2()
-        picam2.configure(picam2.create_still_configuration())
-        picam2.start()
-        time.sleep(1)
-        array = picam2.capture_array("main")
-        return f"{array.shape}"
-    except Exception as e:
-        return e
+    photo = take_photo()
+    return f"{photo.shape}"
 
 
 if __name__ == "__main__":
