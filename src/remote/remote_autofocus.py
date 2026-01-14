3b23f0e7ccda1e76666cb5842a106605c074f83c
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2026-01-14 15:47:19 +0100

    Take photo

--- src/remote/remote_autofocus.py
+++ src/remote/remote_autofocus.py
@@ -1,11 +1,21 @@
 from flask import Flask, Response
+from picamera2 import Picamera2
+import time
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
-def ping_route():
-    return "Hello World"
+@app.route("/rate-sharpness", methods=["GET"])
+def rate_sharpness_route():
+    try:
+        picam2 = Picamera2()
+        picam2.configure(picam2.create_still_configuration())
+        picam2.start()
+        time.sleep(1)
+        array = picam2.capture_array("main")
+        return f"{array.shape}"
+    except Exception as e:
+        return e
 
 
 if __name__ == "__main__":
