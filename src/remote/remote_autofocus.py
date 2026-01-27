fc47ba73aa3d6eda67aad4f25974916cd85252fe
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2026-01-27 10:32:23 +0100

    Update docstring

--- src/remote/remote_autofocus.py
+++ src/remote/remote_autofocus.py
@@ -1,10 +1,14 @@
 """
-Finds the sharpest image in a list of images to help automatically focus camera
-lenses. At least one printed fiducial marker (marker.pdf) must be visible in
-each image without any obstruction at the distance that should be in focus.
-The largest marker is recognized using the Aruco markers located at each corner
+Automatically focus a camera lens.
+At least one printed fiducial marker (marker.pdf) must be visible to the camera
+without any obstruction at the distance that should be in focus.
+The closest marker is recognized using the Aruco markers located at each corner
 and cropped out. The sharpness is rated using the Laplacian variance of that
-cropped image.
+cropped image. The optimization process consists of two stages. First, an upper
+and lower bound for the lens position is established by identifying the two
+sharpest pictures in a focus sequence of five pictures with a focal distance
+between 50cm and infinity. Second, A binary search between the the two
+corresponding lens positions (hopefully) converges to the optimum.
 """
 
 from flask import Flask, jsonify
